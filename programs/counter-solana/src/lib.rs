090f461e73ea8f674102d4ed6bb2df11808bead2
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-11 07:28:35 +0530

    built and tested successfully

--- programs/counter-solana/src/lib.rs
+++ programs/counter-solana/src/lib.rs
@@ -1,15 +1,46 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7JHcUJmYyN4Qto1i3SsYLY28f9QQcYB8SyxzniyQ32mc");
+declare_id!("ETQbUYXC6codoF79uGx4CmC3aLJ1BiSs6gLd8ZQFZa2C");
 
 #[program]
 pub mod counter_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_counter(_ctx: Context<InitializeCounter>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter.count += 1;
+        msg!("Count is {}", ctx.accounts.counter.count);
         Ok(())
     }
 }
 
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
+
+impl Counter {
+    const LEN: usize = 8 + 8;
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeCounter<'info> {
+    #[account(
+        init,
+        payer = payer,
+        space = Counter::LEN,
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
